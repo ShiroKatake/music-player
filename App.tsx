717f9659b23ee95222dee579c644692ee5084dea
ShiroKatake <khang.trinh@movember.com>
2021-11-14 13:38:06 +1100

    ADD: Vector icons

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 const App = () => {
   return (
     <View style={styles.container}>
+      <Ionicons name="heart-outline" size={30} />
       <Text>Hello World!</Text>
     </View>
   );
