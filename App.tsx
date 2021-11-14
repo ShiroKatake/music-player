8eb72ac36bd5f8bce7e32385bef42440b599a8cd
ShiroKatake <khang.trinh@movember.com>
2021-11-14 13:58:20 +1100

    UPDATE: Added music control bar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
-import Ionicons from "react-native-vector-icons/Ionicons";
-import Slider from "@react-native-community/slider";
+import { View, StatusBar, StyleSheet } from "react-native";
 import MusicPlayer from "./components/MusicPlayer";
 
 const App = () => {
   return (
     <View style={styles.container}>
+      <StatusBar barStyle="light-content" />
       <MusicPlayer />
     </View>
   );
--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,15 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import { View, Text, StyleSheet, SafeAreaView, Dimensions } from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Slider from "@react-native-community/slider";
+
+const { width, height } = Dimensions.get("window");
 
 const MusicPlayer = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContainer}>
-        <Ionicons name="heart-outline" size={30} />
-        <Text>Hello World!</Text>
         <Slider />
+      </View>
+      <View style={styles.navContainer}>
+        <View style={styles.buttonContainer}>
+          <Ionicons name="heart-outline" size={30} color="#777" />
+          <Ionicons name="repeat-outline" size={30} color="#777" />
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -24,6 +30,18 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  navContainer: {
+    borderTopColor: "#393E46",
+    borderTopWidth: 1,
+    width: width,
+    alignItems: "center",
+    paddingVertical: 15,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
   },
 });
 
