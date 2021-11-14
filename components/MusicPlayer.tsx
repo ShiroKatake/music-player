81b65a149be8bac56a661d1b6afd337c0a1df1b4
ShiroKatake <khang.trinh@movember.com>
2021-11-14 14:48:32 +1100

    UPDATE: Added track image

--- components/MusicPlayer.tsx
+++ components/MusicPlayer.tsx
@@ -1,5 +1,13 @@
 import React from "react";
-import { View, Text, StyleSheet, SafeAreaView, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Slider from "@react-native-community/slider";
 
@@ -9,12 +17,27 @@ const MusicPlayer = () => {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.mainContainer}>
-        <Slider />
+        <View style={styles.artworkWrapper}>
+          <Image
+            style={styles.artworkImage}
+            source={{
+              uri: "https://upload.wikimedia.org/wikipedia/en/4/45/Divide_cover.png",
+            }}
+          />
+        </View>
       </View>
       <View style={styles.navContainer}>
         <View style={styles.buttonContainer}>
-          <Ionicons name="heart-outline" size={30} color="#777" />
-          <Ionicons name="repeat-outline" size={30} color="#777" />
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="heart-outline" size={30} color="#777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="repeat" size={30} color="#777" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => {}}>
+            <Ionicons name="share-outline" size={30} color="#777" />
+          </TouchableOpacity>
+          <Ionicons name="ellipsis-horizontal" size={30} color="#777" />
         </View>
       </View>
     </SafeAreaView>
@@ -24,7 +47,7 @@ const MusicPlayer = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#222831",
+    backgroundColor: "#222",
   },
   mainContainer: {
     flex: 1,
@@ -42,6 +65,29 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     width: "80%",
+  },
+  artworkWrapper: {
+    width: 300,
+    height: 300,
+    marginBottom: 25,
+
+    //Shadow for iOS
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 5,
+      height: 5,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 3.84,
+
+    //Shadow for Android
+    borderRadius: 15,
+    elevation: 10,
+  },
+  artworkImage: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 15,
   },
 });
 
